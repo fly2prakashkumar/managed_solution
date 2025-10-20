5e16f4cecfb1c918f425d77bf6ecc395c117176d
Prakash S k <fly2prakashkumar@gmail.com>
2025-10-20 03:40:07 +0000

    remove (_for element <Header>_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Toaster } from "@/components/ui/toaster";
-import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 import "./globals.css";
 
@@ -26,7 +25,6 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         <div className="flex min-h-screen flex-col">
-          <Header />
           <main className="flex-grow">{children}</main>
           <Footer />
         </div>
